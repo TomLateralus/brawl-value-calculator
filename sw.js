83666f6ff0e8f34ecb6a2a1487dad87cb5be4dff
TomLateralus <TomLateralus@gmail.com>
2026-03-25 16:32:01 +1100

    Add files via upload
    Version 7

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-const CACHE_NAME = "brawl-calc-v2";
-const urlsToCache = ["./", "index.html"];
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('fetch', function(e){});
