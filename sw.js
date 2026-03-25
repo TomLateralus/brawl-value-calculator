1ec13968be0c03ac4e9f38d3cad010220b25e513
TomLateralus <TomLateralus@gmail.com>
2026-03-25 22:14:26 +1100

    Add files via upload
    Version 9

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-self.addEventListener('fetch', function(e){});
+const CACHE_NAME = "brawl-v9-cache";
+const urlsToCache = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
